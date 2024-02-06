2d9a46b89e208a2d2479c4e73b14f90ee05ed1e0
4hoe8pow <124800114+4hoe8pow@users.noreply.github.com>
2024-02-06 14:46:07 +0900

    Update git.lua

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,23 +1,42 @@
-return  {
-    "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-}
+local h = require("util.helper")
 
 return {
-    'lewis6991/gitsigns.nvim',
-    event = {'BufNewFile', 'BufRead'},
-    keys = {
-      { '<C-j>d', ':Gitsigns preview_hunk<CR>' },
-      { '<C-j>D', ':Gitsigns diffthis<CR>' },
-      { '<C-j><C-u>', ':Gitsigns reset_hunk<CR>' },
-      { '<Space>s', ':Gitsigns stage_hunk<CR>' },
-      { '<Space>j', ':Gitsigns next_hunk<CR>' },
-      { '<Space>k', ':Gitsigns prev_hunk<CR>' },
-    },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPost",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    init = function()
+      h.nmap("<Leader>hp", "<CMD>Gitsigns preview_hunk<CR>", { desc = "Show preview the hunk" })
+
+      for _, mode in pairs({ "n", "v" }) do
+        local key = mode .. "map"
+        h[key]("<Leader>hs", "<CMD>Gitsigns stage_hunk<CR>", { desc = "Stage the hunk" })
+        h[key]("<Leader>hu", "<CMD>Gitsigns undo_stage_hunk<CR>", { desc = "Undo the last call of stage hunk" })
+        h[key]("<Leader>hr", "<CMD>Gitsigns reset_hunk<CR>", { desc = "Reset the lines of the hunk" })
+      end
+    end,
     config = function()
-      require('gitsigns').setup()
-    end
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "+" },
+          change = { text = "*" },
+          delete = { text = "-" },
+          topdelete = { text = "-" },
+          changedelete = { text = "*" },
+        },
+        preview_config = {
+          border = "single",
+          style = "minimal",
+        },
+      })
+    end,
+  },
+  {
+    "rhysd/git-messenger.vim",
+    cmd = "GitMessenger",
+    init = function()
+      h.nmap("<C-k>", "<CMD>GitMessenger<CR>", { desc = "Show git blame on the current line" })
+      vim.g.git_messenger_floating_win_opts = { border = "single" }
+    end,
+  },
 }
