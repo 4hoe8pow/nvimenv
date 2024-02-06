2ecd02abe30cd1fe4137f308a98b15659a11be57
4hoe8pow <124800114+4hoe8pow@users.noreply.github.com>
2024-02-06 15:37:03 +0900

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,17 @@ local keymap = vim.api.nvim_set_keymap
 
 -- leader
 vim.g.mapleader = " "
+
+-- Save file by CTRL-S
+keymap("n", "<C-s>", ":w<CR>", silent)
+
+-- Don't yank on delete char
+keymap("n", "x", '"_x', silent)
+keymap("n", "X", '"_X', silent)
+keymap("v", "x", '"_x', silent)
+keymap("v", "X", '"_X', silent)
+
+vim.cmd[[
+command! -nargs=* T split | wincmd j | resize 20 | terminal <args>
+tnoremap jj <c-\><c-n>
+]]
