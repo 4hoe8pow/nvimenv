0bbdf459db99caa909e558986e5cdae2aac60e06
4hoe8pow <124800114+4hoe8pow@users.noreply.github.com>
2024-02-06 15:29:21 +0900

    Update autocmds.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+
+-- Disable diagnostics in node_modules (0 is current buffer only)
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
+
+-- Show `` in specific files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.txt", "*.md", "*.json" },
+  command = "setlocal conceallevel=0" })
+
