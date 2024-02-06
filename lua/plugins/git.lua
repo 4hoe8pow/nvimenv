3f5fd6574232e7953dfdafa1047b8a7326d348a4
4hoe8pow <124800114+4hoe8pow@users.noreply.github.com>
2024-02-06 14:27:49 +0900

    Update git.lua

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,3 +5,19 @@ return  {
         "nvim-lua/plenary.nvim",
     },
 }
+
+return {
+    'lewis6991/gitsigns.nvim',
+    event = {'BufNewFile', 'BufRead'},
+    keys = {
+      { '<C-j>d', ':Gitsigns preview_hunk<CR>' },
+      { '<C-j>D', ':Gitsigns diffthis<CR>' },
+      { '<C-j><C-u>', ':Gitsigns reset_hunk<CR>' },
+      { '<Space>s', ':Gitsigns stage_hunk<CR>' },
+      { '<Space>j', ':Gitsigns next_hunk<CR>' },
+      { '<Space>k', ':Gitsigns prev_hunk<CR>' },
+    },
+    config = function()
+      require('gitsigns').setup()
+    end
+}
