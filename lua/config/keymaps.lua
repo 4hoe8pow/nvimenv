17f7aaf6dbccc72ac39220b7f4371d2495fe3aff
4hoe8pow <124800114+4hoe8pow@users.noreply.github.com>
2024-02-06 13:37:04 +0900

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.api.nvim_set_keymap
+
+-- leader
+vim.g.mapleader = " "
+
+-- fern
+keymap("n", "<leader>e", "<cmd>Fern . -reveal=% -drawer -toggle -width=40<CR>", opts)
+keymap("n", "<leader>o", "", {
+  callback = function()
+    if vim.bo.filetype == "fern" then
+      vim.cmd.wincmd "p"
+    else
+      vim.cmd.Fern(".", "-reveal=%", "-drawer", "-width=40")
+    end
+  end,
+  noremap = true,
+  silent = true,
+})
