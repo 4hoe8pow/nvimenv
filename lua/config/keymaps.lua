4118754e7ef97f9c7d49957b94de5b3d4704ae8c
4hoe8pow <124800114+4hoe8pow@users.noreply.github.com>
2024-02-06 14:28:07 +0900

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,17 +4,3 @@ local keymap = vim.api.nvim_set_keymap
 
 -- leader
 vim.g.mapleader = " "
-
--- fern
-keymap("n", "<leader>e", "<cmd>Fern . -reveal=% -drawer -toggle -width=40<CR>", opts)
-keymap("n", "<leader>o", "", {
-  callback = function()
-    if vim.bo.filetype == "fern" then
-      vim.cmd.wincmd "p"
-    else
-      vim.cmd.Fern(".", "-reveal=%", "-drawer", "-width=40")
-    end
-  end,
-  noremap = true,
-  silent = true,
-})
