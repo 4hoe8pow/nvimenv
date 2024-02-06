69e2ba0055831a084da2f843a7fe58286122e965
4hoe8pow <124800114+4hoe8pow@users.noreply.github.com>
2024-02-06 15:51:28 +0900

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
+local silent = { silent = true }
 
 -- leader
 vim.g.mapleader = " "
